f94589132b55717ef237011400e9aa955aa860af
Einārs Lielmanis <einars@spicausis.lv>
2021-12-09 20:30:46 +0200

    Refactor for clarity (?)

--- code-09.clj
+++ code-09.clj
@@ -2,7 +2,7 @@
 
 (defn read-heightmap-line [accu [line y]]
   (let [line-and-x (map list line (range))]
-    (reduce (fn [tmp-accu [ depth x ]] (assoc tmp-accu (list x y) (Integer/parseInt (str depth)) ))
+    (reduce (fn [tmp-accu [depth x]] (assoc tmp-accu (list x y) (Integer/parseInt (str depth)) ))
             accu 
             line-and-x)))
 
@@ -14,32 +14,34 @@
 
 (defn find-low-points [m]
   (filter (fn [coord] 
-            (every?
-              (fn [d] (> d (get m coord)))
-              (map #(get m % 10) (neighbor-coords coord))))
+            (every? #(> % (get m coord))
+                    (filter some? (map m (neighbor-coords coord)))))
           (keys m)))
 
-(defn solve-a [m]
-  (let [coords (find-low-points m)
-        depths (map #(get m %) coords)
-        risks (map inc depths)]
-    (reduce + risks)))
-
 (defn size-of-basin [m accum]
-  (let [expanse (->> (reduce concat (mapv neighbor-coords accum)) ; all neighbors
+  (let [expanse (->> (reduce concat (map neighbor-coords accum)) ; all neighbors
                      (filter #(nil? (get accum %))) ; only new
                      (filter #(some? (get m %))) ; only actually on map
                      (filter #(< (get m %) 9)) ; not walls
                      (concat accum)
                      (set))]
-    (if (= (count expanse) (count accum))
+    (if (= expanse accum)
       (count expanse)
       (recur m expanse))))
 
+(defn solve-a [m]
+  (->> (find-low-points m)
+       (map m)
+       (map inc)
+       (reduce +)))
+
 (defn solve-b [m]
-  (let [coords (find-low-points m)
-        sizes (mapv #(size-of-basin m (set (list %))) coords)]
-    (reduce * (take 3 (sort > sizes)))))
+  (->> (find-low-points m)
+       (map #(size-of-basin m (set [%])))
+       (sort >)
+       (take 3)
+       (reduce *)))
+
 
 (defn read-heightmap [file]
   (reduce read-heightmap-line {} (map list (string/split (slurp file) #"\n") (range))))
