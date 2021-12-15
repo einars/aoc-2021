f92a71dd4b5c2fdaa157e631d341f2237a7f23bb
Einārs Lielmanis <einars@spicausis.lv>
2021-12-15 21:16:08 +0200

    minor refactor/styling

--- code-15.clj
+++ code-15.clj
@@ -11,47 +11,46 @@
 (defn read-xy-map [file]
   (reduce merge (map make-coordinate-map (map list (str/split (slurp file) #"\n") (range)))))
 
-
 (defn neighbors [[x y]]
   (list [(inc x) y]
         [x (inc y)]
         [(dec x) y]
         [x (dec y)]))
 
-
 (defn improve-around-neighbors [m best-paths accu pt]
   (->> (neighbors pt)
        (filter m) ; neighbors on map
        (map #(list % (+ (m %) (best-paths pt)))) ; neighbor list of [point score]
-       (filter (fn [[pt score]]  
-                 (and
-                   (or (nil? (best-paths pt)) ; leave only new or improved
-                       (< score (best-paths pt)))
-                   (or (nil? (accu pt))
-                       (< score (accu pt))))))
+       (filter (fn [[pt score]]
+                 (and (or (nil? (best-paths pt)) ; leave only new or improved
+                          (< score (best-paths pt)))
+                      (or (nil? (accu pt))
+                          (< score (accu pt))))))
        (reduce #(assoc %1 (first %2) (second %2)) accu)))
 
-
-(defn trace-path 
+(defn trace-path
   ([m] (trace-path m (assoc {} [0 0] 0) [[0 0]] ))
-  ([m best-paths front]
-   (let [new-front (reduce #(improve-around-neighbors m best-paths %1 %2) {} front)]
-     (do (prn "new-front" (count new-front))
-         (if (empty? new-front)
+
+  ([m best-paths frontier]
+   (let [new-frontier (reduce (partial improve-around-neighbors m best-paths)
+                              {}
+                              frontier)]
+     (do (printf ".")
+         (flush)
+         (if (empty? new-frontier)
            best-paths
            (recur m
-                  (merge best-paths new-front)
-                  (keys new-front)))))))
+                  (merge best-paths new-frontier)
+                  (keys new-frontier)))))))
 
-(defn bottom-right [area] [(apply max (map first (keys area)))
-                           (apply max (map second (keys area)))])
+(defn bottom-right [area] 
+  (list (apply max (map first (keys area)))
+        (apply max (map second (keys area)))))
 
 (defn at-bottom-right [area] (area (bottom-right area)))
 
 (defn clamp-risk [r]
-  (if (> r 9)
-    (clamp-risk (- r 9))
-    r))
+  (if (> r 9) (- r 9) r))
 
 (defn build-huge-map [m]
   (let [[sx sy] (bottom-right m)
@@ -61,9 +60,10 @@
                         (map (fn [[x y]] (list (list (+ x (* (inc sx) rx))
                                                      (+ y (* (inc sy) ry)))
                                                (clamp-risk (+ (m [x y]) rx ry))))
-                             all-keys))
-        all-extra (reduce concat extra-squares)]
-    (reduce (fn [accu [pt risk]] (assoc accu pt risk)) {} all-extra)))
+                             all-keys))]
+    (reduce #(apply assoc %1 %2)
+            {}
+            (reduce concat extra-squares))))
 
 (defn print-map [m]
   (let [[sx sy] (bottom-right m)]
@@ -72,15 +72,14 @@
              (do
                (when (= x 0)
                  (printf "\n"))
-                 (printf "%d" (m [x y] "-")))))))
+               (printf "%d" (m [x y] "-")))))))
 
-
-
+; ---
 
 (assert (= 40 (at-bottom-right (trace-path (read-xy-map "data/test-15.txt")))))
 (assert (= 315 (at-bottom-right (trace-path (build-huge-map (read-xy-map "data/test-15.txt"))))))
 
-;(prn (at-bottom-right (trace-path (read-xy-map "data/input-15.txt"))))
+(prn (at-bottom-right (trace-path (read-xy-map "data/input-15.txt"))))
 (prn (at-bottom-right (trace-path (build-huge-map (read-xy-map "data/input-15.txt")))))
 
 ;(print-map (build-huge-map (read-xy-map "data/test-15.txt")))
