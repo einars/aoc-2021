17be9c52398df6bf516eeeb3a1f1e9f17bcc1d8f
Einārs Lielmanis <einars@spicausis.lv>
2021-12-17 15:36:18 +0200

    Very, very hackish day 17 part 2

--- code-17.clj
+++ code-17.clj
@@ -15,7 +15,7 @@
     (and (< dy 0) (< y ymin) (> x xmax)) :overshoot
     (and (< dy 0) (< y ymin)) :miss
     :else
-    (iterate-shoot [(+ x dx) (+ y dy)]
+    (recur [(+ x dx) (+ y dy)]
                    [(towards-0 dx) (dec dy)]
                    [xmin xmax]
                    [ymin ymax]
@@ -29,9 +29,7 @@
 
 (defn count-best [results]
   (let [res (filter (complement keyword?) results)]
-    (if (empty? res)
-      nil
-      (apply max res))))
+    (count res)))
 
 (defn shoot-y [y [adjx adjy] xrange yrange]
   (let [ra (iterate-shoot [0 0] [adjx y] xrange yrange 0)
@@ -46,15 +44,15 @@
       ;:not-enough
 
       :else
-      (take-best (for [x (range adjx adjy)]
-        (iterate-shoot [0 0] [x y] xrange yrange y))))))
+      (count-best (for [x (range adjx adjy)]
+        (iterate-shoot [0 0] [x y] xrange yrange 0))))))
 
   
 
 
 
 (defn solve [xrange yrange y current-best]
-  (let [res (shoot-y y [-100 100] xrange yrange)]
+  (let [res (shoot-y y [-400 400] xrange yrange)]
     (do
       (printf "%d %s, cur-best %d\n" y res current-best)
       (condp = res
@@ -68,7 +66,7 @@
         nil
         (recur xrange yrange (inc y) current-best)
 
-        (recur xrange yrange (inc y) (max current-best res))))))
+        (recur xrange yrange (inc y) (+ current-best res))))))
 
-;(prn (solve [20 30] [-10 -5] 0 0))
-(prn (solve [70 125] [-159 -121] 0 0))
+;(prn (solve [20 30] [-10 -5] -20 0))
+(prn (solve [70 125] [-159 -121] -1500 0))
