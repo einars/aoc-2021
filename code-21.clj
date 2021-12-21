a94f4da90bffd54f2ff983c57833747fe4749bfd
Einārs Lielmanis <einars@spicausis.lv>
2021-12-21 12:19:05 +0200

    Day 21, pt 2 done

--- code-21.clj
+++ code-21.clj
@@ -29,6 +29,68 @@
   (let [[win lose n-rolls] (play-with [p1 0] [p2 0] (new-dice))]
     (* lose n-rolls)))
 
+
+
+(def dirac-play-p1)
+(def dirac-play-p2)
+
+(defn memo-dirac-play-p1 [p1-space p1-score p2-space p2-score]
+  (cond
+    (>= p1-score 21) [1 0]
+    (>= p2-score 21) [0 1]
+    :else 
+    (let [m1 (clamp-one (+ p1-space 1) 10)
+          m2 (clamp-one (+ p1-space 2) 10)
+          m3 (clamp-one (+ p1-space 3) 10)]
+      (map + 
+           (dirac-play-p2 m1
+                          (+ p1-score m1)
+                          p2-space
+                          p2-score)
+           (dirac-play-p2 m2
+                          (+ p1-score m2)
+                          p2-space
+                          p2-score)
+           (dirac-play-p2 m3
+                          (+ p1-score m3)
+                          p2-space
+                          p2-score)))))
+
+
+(def next-move
+  {:roll-1-1 :roll-1-2
+   :roll-1-2 :roll-1-3
+   :roll-1-3 :roll-2-1
+   :roll-2-1 :roll-2-2
+   :roll-2-2 :roll-2-3
+   :roll-2-3 :roll-1-1})
+
+(def dirac-play)
+(defn memo-dirac-play [move p1-space p1-score p2-space p2-score]
+  (cond
+    (>= p2-score 21) [0 1]
+    (>= p1-score 21) [1 0]
+
+    (or (= move :roll-1-1) (= move :roll-1-2) (= move :roll-1-3))
+    (let [m1 (clamp-one (+ p1-space 1) 10)
+          m2 (clamp-one (+ p1-space 2) 10)
+          m3 (clamp-one (+ p1-space 3) 10)]
+      (map + 
+           (dirac-play (next-move move) m1 (if (= move :roll-1-3) (+ p1-score m1) p1-score) p2-space p2-score)
+           (dirac-play (next-move move) m2 (if (= move :roll-1-3) (+ p1-score m2) p1-score) p2-space p2-score)
+           (dirac-play (next-move move) m3 (if (= move :roll-1-3) (+ p1-score m3) p1-score) p2-space p2-score)))
+    (or (= move :roll-2-1) (= move :roll-2-2) (= move :roll-2-3))
+    (let [m1 (clamp-one (+ p2-space 1) 10)
+          m2 (clamp-one (+ p2-space 2) 10)
+          m3 (clamp-one (+ p2-space 3) 10)]
+      (map + 
+           (dirac-play (next-move move) p1-space p1-score m1 (if (= move :roll-2-3) (+ p2-score m1) p2-score))
+           (dirac-play (next-move move) p1-space p1-score m2 (if (= move :roll-2-3) (+ p2-score m2) p2-score))
+           (dirac-play (next-move move) p1-space p1-score m3 (if (= move :roll-2-3) (+ p2-score m3) p2-score))))))
+
+(def dirac-play (memoize memo-dirac-play))
+
+
 ; ---
 
 (let [d (new-dice)]
@@ -47,4 +109,6 @@
 
 (prn (solve-a 8 10))
 
+;(prn (dirac-play :roll-1-1 4 0 8 0))
+(prn (apply max (dirac-play :roll-1-1 8 0 10 0)))
 
