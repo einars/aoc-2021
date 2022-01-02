da57fcca8390bd84e201973ff109894de4aa4006
Einārs Lielmanis <einars@spicausis.lv>
2022-01-02 21:31:15 +0200

    day 23 pt 1, slow

--- code-23.clj
+++ code-23.clj
@@ -1,111 +1,199 @@
 (require '[clojure.test :refer [are is]])
 (require '[clojure.tools.trace :use all])
 
-(def rooms
-  {0  {:nbs [1],      :home nil, :hallway true}
-   1  {:nbs [0 2],    :home nil, :hallway true}
-   3  {:nbs [2 4],    :home nil, :hallway true}
-   5  {:nbs [4 6],    :home nil, :hallway true}
-   7  {:nbs [6 8],    :home nil, :hallway true}
-   9  {:nbs [8 10],   :home nil, :hallway true}
-   10 {:nbs [9],      :home nil, :hallway true}
-   11 {:nbs [[1] [3] 12], :home 'a}
-   12 {:nbs [11],         :home 'a, :endpoint true}
-   13 {:nbs [[3] [5] 14], :home 'b}
-   14 {:nbs [13],         :home 'b, :endpoint true}
-   15 {:nbs [[5] [7] 16], :home 'c}
-   16 {:nbs [15],         :home 'c, :endpoint true}
-   17 {:nbs [[7] [9] 18], :home 'd}
-   18 {:nbs [17],         :home 'd, :endpoint true}
+(require '[shams.priority-queue :as pq])
+
+(def rooms ; global. if it changes in part 2, i'll deal w/that then
+  {1  {:id 1,  :nbs [2],       :home 'hallway}
+   2  {:id 2,  :nbs [1 3],     :home 'hallway}
+   3  {:id 3,  :nbs [2 4 12],  :home 'hallway, :move-through true}
+   4  {:id 4,  :nbs [3 5],     :home 'hallway}
+   5  {:id 5,  :nbs [4 6 14],  :home 'hallway, :move-through true}
+   6  {:id 6,  :nbs [5 7],     :home 'hallway}
+   7  {:id 7,  :nbs [6 8 16],  :home 'hallway, :move-through true}
+   8  {:id 8,  :nbs [7 9],     :home 'hallway}
+   9  {:id 9,  :nbs [8 10 18], :home 'hallway, :move-through true}
+   10 {:id 10, :nbs [9 11],    :home 'hallway}
+   11 {:id 11, :nbs [10],      :home 'hallway}
+   12 {:id 12, :nbs [3 13],    :home 'a}
+   13 {:id 13, :nbs [12],      :home 'a, :endpoint true}
+   14 {:id 14, :nbs [5 15],    :home 'b}
+   15 {:id 15, :nbs [14],      :home 'b, :endpoint true}
+   16 {:id 16, :nbs [7 17],    :home 'c}
+   17 {:id 17, :nbs [16],      :home 'c, :endpoint true}
+   18 {:id 18, :nbs [9 19],    :home 'd}
+   19 {:id 19, :nbs [18],      :home 'd, :endpoint true}
    })
 
-(def test-state-0
-  [{:me 'b, :in 11, :spent 0, :cost 1}
-   {:me 'a, :in 12, :spent 0, :cost 1}
-   {:me 'c, :in 13, :spent 0, :cost 10}
-   {:me 'd, :in 14, :spent 0, :cost 10}
-   {:me 'b, :in 15, :spent 0, :cost 100}
-   {:me 'c, :in 16, :spent 0, :cost 100}
-   {:me 'd, :in 17, :spent 0, :cost 1000}
-   {:me 'a, :in 18, :spent 0, :cost 1000}])
+(def pod-state-0
+  {0 {:me 'a, :in 12, :spent 0, :cost 1}
+   1 {:me 'a, :in 17, :spent 0, :cost 1}
+   2 {:me 'b, :in 16, :spent 0, :cost 10}
+   3 {:me 'b, :in 18, :spent 0, :cost 10}
+   4 {:me 'c, :in 14, :spent 0, :cost 100}
+   5 {:me 'c, :in 19, :spent 0, :cost 100}
+   6 {:me 'd, :in 13, :spent 0, :cost 1000}
+   7 {:me 'd, :in 15, :spent 0, :cost 1000}})
 
-(defn unoccupied-rooms [room_ids state]
-  (filter #(every? (fn [st] (not= (:in st) %)) state) room_ids))
+(defn open-areas [current-id pods visited-set]
+  (let [room (rooms current-id)
+        taken-rooms (set (mapv :in (vals pods)))]
+    (->> (rooms current-id)
+         :nbs
+         (filter (complement visited-set))
+         (filter (complement taken-rooms)))))
 
-(is (= [9 10] (unoccupied-rooms [9 10 11 12] test-state-0)))
+(defn unfiltered-destinations 
+  "returns a list of [room-id, steps] that this pod can move to"
+  ([pod pods] (unfiltered-destinations (:in pod) pods #{}))
+  ([headroom-id pods visited-set]
+   (if-let [destination-ids (open-areas headroom-id pods visited-set)]
+     (reduce into
+             (mapv (fn [d-id] (list d-id (inc (count visited-set))) ) destination-ids)
+             (mapv #(unfiltered-destinations % pods (conj visited-set %)) destination-ids))
+     [])))
+
+(defn room-available? [pod-type pods]
+  (->>
+    (vals rooms)
+    (filter #(= pod-type (:home %)))
+    (map :id)
+    (map (fn [room-id] (some #(when (= room-id (:in %)) (:me %)) (vals pods))))
+    (every? (fn [maybe-pod-type]
+              (or (nil? maybe-pod-type)
+                  (= maybe-pod-type pod-type))))))
 
 
-(defn foreign-colors-in-my-room? [me others rooms]
-  (some (fn [o] (and (not= me (:me o))
-                     (= me (:home (rooms (:in o))))) others)))
+(defn can-move-to? [pod dest-id pods]
+  (let [s-me (:me pod)
+        source (rooms (:in pod))
+        dest (rooms dest-id)
+        type-dest (:home dest)]
+    (cond
+      ; do not stop in hallway blocker points
+      (true? (:move-through dest))
+      false
+
+      ; do not enter other rooms
+      (and
+        (not= type-dest 'hallway)
+        (not= type-dest s-me))
+      false
+
+      ; do not move from hallway into anything else than own empty room:
+      ; 1. no move from hallway to anything that's not my own room
+      (and
+        (= 'hallway (:home source))
+        (not= s-me (:home dest)))
+      false
+
+      ; 2. do not move to a room that's not yet empty
+      (and
+        (= s-me type-dest)
+        (not (room-available? s-me pods)))
+      false
+
+      ; 2. do not leave own room unless absolutely required
+      (and
+        (= s-me (:home source))
+        (room-available? s-me pods))
+      false
+
+      :else
+      true)))
+
+(defn is-winning? [pos]
+  (every? (fn [pod] (= (:me pod) (:home (rooms (:in pod))))) (vals pos)))
 
 
+(defn filtered-destinations [pod pods]
+  (filterv #(can-move-to? pod (first %) pods) (unfiltered-destinations pod pods)))
 
-(defn available-moves
-  ([state rooms] (mapcat #(available-moves % (filter (partial not= %) state) rooms) state))
-  ([self others rooms]
-   (let [this-room (rooms (:in self))
-         unoccupied (unoccupied-rooms (:nbs this-room) others)]
-     (loop [nbs unoccupied
-            accu []]
-       (if (empty? nbs)
-         accu
-         (let [nb-room (first nbs)
-               ; list syntax for neighboring room means that it is a double-step room
-               mult (if (coll? nb-room) 2 1)
-               nb-room-id (if (coll? nb-room) (first nb-room) nb-room)
-               nb-room (rooms nb-room-id)]
-           (cond
-             (and
-               (= (:home this-room) (:me self))
-               (:endpoint this-room))
-             ; do not move out of own endpoint
-             (recur (rest nbs) accu)
+(defn move-to [entry dest steps]
+  (update
+    (assoc entry :in dest)
+    :spent
+    ;(fn [e] (+ e steps))))
+    (fn [e] (+ e (* (:cost entry) steps)))))
 
-             ;(and
-             ;  (= (:home this-room) (:me self))
-             ;  (nil? (:endpoint nb-room)))
-             ;; do not move out of own room
-             ;(do (prn "no-move-out-of-own")
-             ;(recur (rest nbs) accu))
+(defn run-pods [pods]
+  (loop [pod-ids (keys pods)
+         accu []]
+    (let [me (pods (first pod-ids))]
+      (if (empty? pod-ids)
+        accu
+        (recur (rest pod-ids)
+               (into accu 
+                     (map (fn [[dest steps]] (update pods (first pod-ids) #(move-to % dest steps))) 
+                          (filtered-destinations me pods))))))))
 
-             (and (:hallway? this-room)
-                  (not= (:home nb-room) (:me self)))
-             ; do not move into other rooms
-             (recur (rest nbs) accu)
+(defn score [pods] (reduce + (map :spent (vals pods))))
+(defn negscore [pods] (- (score pods)))
 
-             (and (:hallway? this-room)
-                  (= (:home nb-room) (:me self))
-                  (foreign-colors-in-my-room? (:me self) others rooms))
-             ; do not move into own room taken by others
-             (recur (rest nbs) accu)
+(defn print-pods [pods]
+  (let [m (into {} (map (fn [p] [(:in p) (:me p)]) (vals pods)))]
+    (printf "%s%s%s%s%s%s%s%s%s%s%s\n"
+          (m 1 ".")
+          (m 2 ".")
+          (m 3 ".")
+          (m 4 ".")
+          (m 5 ".")
+          (m 6 ".")
+          (m 7 ".")
+          (m 8 ".")
+          (m 9 ".")
+          (m 10 ".")
+          (m 11 "."))
+    (printf "  %s %s %s %s\n"
+            (m 12 ".")
+            (m 14 ".")
+            (m 16 ".")
+            (m 18 "."))
+    (printf "  %s %s %s %s\n"
+            (m 13 ".")
+            (m 15 ".")
+            (m 17 ".")
+            (m 19 "."))))
 
-             ;todo: do not move into room if it contains 
+(defn repr [pods] (mapv :in (vals pods)))
 
-             :else
-             (recur (rest nbs) 
-                    (conj accu
-                          (conj others {:me (:me self)
-                                        :in nb-room-id
-                                        :spent (+ (:spent self) (* mult (:cost self)))
-                                        :cost (:cost self)
-                                        }))))))))))
-
-(defn is-winning-state? [state rooms]
-  (every? (fn [me] (= (:me me) 
-                      (:home (rooms (:in me)))))
-          state))
-
-(defn advance-state [states rooms]
-  (let [new-states (mapcat #(available-moves % rooms) states)
-        winners (filter #(is-winning-state? % rooms) new-states)]
-    (assert (seq new-states))
-    (prn (count new-states))
-    (if (empty? winners)
-      (recur new-states rooms)
+(defn run-pool [pq-pool seen]
+  (let [pods (peek pq-pool)
+        r (repr pods)
+        new-states (filter #(not (seen (repr %))) (run-pods pods))]
+    (if (seen r)
+      (recur (pop pq-pool) seen)
       (do
-        (prn winners)
-        (assert false)))))
+        (prn (score pods) (count pq-pool) (count seen))
+        (print-pods pods)
+        (if (is-winning? pods)
+          (prn "WINNING" pods (score pods))
+          (recur (into (pop pq-pool) new-states)
+                 (conj seen r)))))))
 
 
-(advance-state [test-state-0] rooms)
+(defn create-pool [pods]
+  (pq/priority-queue negscore :elements [pods]))
+
+
+(prn (run-pool (create-pool pod-state-0) #{}))
+;(prn (filtered-destinations (first pod-state-0) pod-state-0))
+
+
+
+(def ptest
+  {0 {:me 'a, :in 12, :spent 0, :cost 10}
+   1 {:me 'a, :in 13, :spent 0, :cost 1}
+   2 {:me 'b, :in 4, :spent 0, :cost 100}
+   3 {:me 'b, :in 16, :spent 0, :cost 1000}
+   4 {:me 'c, :in 14,  :spent 0, :cost 10}
+   5 {:me 'c, :in 17, :spent 0, :cost 100}
+   6 {:me 'd, :in 1, :spent 0, :cost 1000}
+   7 {:me 'd, :in 15, :spent 0, :cost 1}})
+
+;(print-pods ptest)
+;(prn (unfiltered-destinations (ptest 3) ptest))
+;(prn (filtered-destinations (ptest 3) ptest))
+
+;(prn (unfiltered-destinations (ptest 2) ptest))
+;(prn (filtered-destinations (ptest 2) ptest))
